093b484a23305360d7aee6ae7cff12a447940e3f
Alexis Moody <amoodyldes@gmail.com>
2016-11-13 03:12:09 +0000

    style config

--- fantasy-congress/app/controllers/home_controller.rb
+++ fantasy-congress/app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @news = HTTParty.get('https://www.google.com/search?hl=en&gl=us&tbm=nws&authuser=0&q=114+congress&oq=114+congress&gs_l=news-cc.3..43j43i53.1446.5470.0.5856.12.4.0.8.8.0.284.536.2j1j1.4.0...0.0...1ac.1.8hSvsVT6kow')
   end
 end
