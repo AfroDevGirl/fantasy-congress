f19b6605e10085e951f6e6e2b28e1cf92c863575
Alexis Moody <amoodyldes@gmail.com>
2016-11-13 17:59:41 +0000

    add year calculation mehod

--- fantasy-congress/app/models/politician.rb
+++ fantasy-congress/app/models/politician.rb
@@ -1,4 +1,13 @@
 class Politician < ApplicationRecord
     has_many :bills
     has_many :votes
+    
+    def years_in_office
+        start = self.start_date.split('-')
+        start_datetime = DateTime.new(start[0].to_i, start[1].to_i, start[2].to_i)
+        now = Date.today
+        
+        days = (now - start_datetime).to_i
+        years = (days/365.25).round.to_s
+    end 
 end
